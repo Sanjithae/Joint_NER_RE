9bff49322ff68c2b76b12687f55001ce5fabb755
sanjithae <44079238+Sanjithae@users.noreply.github.com>
2019-05-01 03:24:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import multhead_joint_entity_relation_extraction
 from multhead_joint_entity_relation_extraction import multihead_joint_entity_relation_extraction
 obj=multihead_joint_entity_relation_extraction()
-filename="/relation_extraction_test_input.txt"
+filename="relation_extraction_test_input.txt"
 data=obj.read_dataset(filename)
 
 obj.data_preprocess(data)
